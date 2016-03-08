596d110e9f96443f7bac91bf10aed0902b0cb107
肖会军 <250154804@qq.com>
2016-03-08 09:18:06 +0800

    Initial Commit

---
+++ MTransparentNavigation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTransparentNavigation
+//
+//  Created by 肖会军 on 16/3/8.
+//  Copyright © 2016年 肖会军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MTransparentNavigation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MTransparentNavigation
+//
+//  Created by 肖会军 on 16/3/8.
+//  Copyright © 2016年 肖会军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
